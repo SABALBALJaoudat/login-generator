4be09ba592af8ade20ea3de6829ecb712b908c3a
SABALBALJaoudat <jaoudat.joudinho@hotmail.fr>
2020-11-26 10:43:06 +0100

    Modif

--- src/test/java/geco/testLoginService.java
+++ src/test/java/geco/testLoginService.java
@@ -1,4 +1,25 @@
 package geco;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class testLoginService {
+    private String[] loginsExistants;
+    LoginService loginService = new LoginService(loginsExistants);
+
+    @Test
+    public void addLogin(){
+        this.loginService.addLogin("cde");
+        assertTrue(this.loginService.loginExists("cde"));
+    }
+
+    @Test
+    public void loginExists(){
+        this.loginService.loginExists("cde");
+        assertTrue(this.loginService.loginExists("cde"));
+        this.loginService.loginExists("cdx");
+        assertFalse(this.loginService.loginExists("cdx"));
+    }
 }
